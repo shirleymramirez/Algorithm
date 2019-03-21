194bbddf7533691aafc0bb904bedacf1f0c07845
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-03-20 22:53:59 -0700

    optimized previous file

--- fraction.js
+++ fraction.js
@@ -13,53 +13,44 @@ function decimalToFraction(cents) {
 
     // sample input 1 cent is equal to $0.01 = 1/51
     // sample input 2 cent is equal to $0.02 = 1/34
-   
-    var parsedValue = parseInt(cents);
     
-    // step 1. check if input is a valid input else return input value;
-    if ( isNaN(parsedValue )){
-        return parsedValue;
-    }
-    else {
-        // step 2. check input if within valid range which is 1 cent to 99 cents)
-        if ( parsedValue >= 1 && parsedValue <= 99) {
+    // step 1. check input if within valid range which is 1 cent to 99 cents)
+    if ( cents >= 1 && cents <= 99) {
 
-            // step 3. cents / 100 = var decimal and round the answer to the nearest 2 digits decimal place
-            var toleranceValue = parseFloat((parsedValue / 100).toString().match(/^\d+(?:\.\d{0,2})?/));  // "0.10"
-            var numerator;
-            var denominator;
+        // step 2. cents / 100 = var decimal and round the answer to the nearest 2 digits decimal place
+        var toleranceValue = parseFloat((cents / 100).toString().match(/^\d+(?:\.\d{0,2})?/)); 
+        var numerator;
+        var denominator;
 
-            // for loop to get the numerator
-            for(var i = 1; i <= parsedValue; i++ ){
-                numerator = i;    // 1
+        // for loop to get the numerator
+        for(var i = 1; i <= cents; i++ ){
+            numerator = i;  
+            
+            // step 3. loop through each decimal starting from 100
+            for (var j = 100; j > 0; j--) {
                 
-                // step 4. loop through each decimal starting from 100
-                for (var j = 100; j > 0; j--) {
-                    
-                    var decimal = parseFloat((numerator/j).toString().match(/^\d+(?:\.\d{0,2})?/));       // 0.01
-                    
-                    //if (cents === 13 && numerator === 1){
-                    //    console.log(`${numerator} ${j} ${decimal} ${toleranceValue}`);
-                    //}
+                var decimal = parseFloat((numerator/j).toString().match(/^\d+(?:\.\d{0,2})?/));  
+                
+                //if (cents === 13 && numerator === 1){
+                //    console.log(`${numerator} ${j} ${decimal} ${toleranceValue}`);
+                //}
 
-                    if ( decimal > toleranceValue ) {
-                        denominator = j + 1;
-                        decimal = parseFloat((numerator / denominator).toString().match(/^\d+(?:\.\d{0,2})?/)); 
-                        if (decimal < toleranceValue ) {
-                            denominator = undefined;
-                        }
-                        break;
+                if ( decimal > toleranceValue ) {
+                    denominator = j + 1;
+                    decimal = parseFloat((numerator / denominator).toString().match(/^\d+(?:\.\d{0,2})?/)); 
+                    if (decimal < toleranceValue ) {
+                        denominator = undefined;
                     }
-                }
-                if(denominator){
                     break;
                 }
-
             }
-
-            const fraction = new Fraction(numerator, denominator, 'dollar');
-            return fraction.toString();
+            if(denominator){
+                break;
+            }
         }
+
+        const fraction = new Fraction(numerator, denominator, 'dollar');
+        return fraction.toString();
     }
 }
 
